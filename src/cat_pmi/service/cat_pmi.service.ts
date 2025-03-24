1782203660016612a4a4e0ceba8b44e1edb6c5ab
Luis Diaz <luisantoniodiaz97@gmail.com>
2025-03-23 20:04:29 -0600

    updat: cat_pmi

--- src/cat_pmi/service/cat_pmi.service.ts
+++ src/cat_pmi/service/cat_pmi.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CatPmi } from '../entity/CatPmi';
 import { Repository } from 'typeorm';
